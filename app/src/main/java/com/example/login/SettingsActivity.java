f7618714a5ec88106243ac49bd6827d719124a13
Angela <angela.nunez@estudiantat.upc.edu>
2023-12-11 15:41:16 +0100

    .

--- app/src/main/java/com/example/login/SettingsActivity.java
+++ app/src/main/java/com/example/login/SettingsActivity.java
@@ -10,7 +10,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_settings);
     }
 
     public void dificilOnClick(View v){
